4a08fe3624d59d47f0f65833e8663b1ecf69a005
Jonarzz <rajek7@gmail.com>
2022-07-06 08:21:15 +0200

    [Unusual Spending kata] Improved exception handling in PaymentRegistrationListener

--- unusual-spending/src/main/java/io/github/jonarzz/kata/unusual/spending/payment/PaymentRegistrationListener.java
+++ unusual-spending/src/main/java/io/github/jonarzz/kata/unusual/spending/payment/PaymentRegistrationListener.java
@@ -3,6 +3,7 @@ package io.github.jonarzz.kata.unusual.spending.payment;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static javax.jms.JMSContext.AUTO_ACKNOWLEDGE;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
@@ -12,6 +13,8 @@ import org.jboss.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.JMSRuntimeException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -60,21 +63,14 @@ class PaymentRegistrationListener implements Runnable {
                 }
                 var messageBody = message.getBody(String.class);
                 LOG.debugf("Polled message from %s queue: %s", queueName, messageBody);
-                var paymentEvent = objectMapper.readValue(messageBody, PaymentRegisteredEvent.class);
-                paymentService.save(paymentEvent);
+                processEvent(messageBody);
             }
-        } catch (Exception exception) {
+        } catch (JMSException | JMSRuntimeException exception) {
             LOG.errorf(exception, "Consumption from %s failed", queueName);
-            if (connectionRetryCount.decrementAndGet() == 0) {
-                throw new IllegalStateException("JMS consumption failed " + MAX_RETRIES + " times, aborting");
-            }
-            try {
-                SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-            LOG.debugf("Retrying connection to %s...", queueName);
-            run();
+            retryConnection();
+        } catch (Exception exception) {
+            LOG.errorf(exception, "Handling an event polled from %s failed", queueName);
+            throw exception;
         }
     }
 
@@ -86,4 +82,29 @@ class PaymentRegistrationListener implements Runnable {
         scheduler.shutdown();
     }
 
+    private void processEvent(String messageBody) {
+        PaymentRegisteredEvent paymentEvent;
+        try {
+            paymentEvent = objectMapper.readValue(messageBody, PaymentRegisteredEvent.class);
+        } catch (JsonProcessingException exception) {
+            LOG.errorf(exception, "Failed to parse");
+            throw new IllegalStateException(exception);
+        }
+        paymentService.save(paymentEvent);
+    }
+
+    private void retryConnection() {
+        if (connectionRetryCount.decrementAndGet() == 0) {
+            var message = "JMS consumption failed " + MAX_RETRIES + " times, aborting";
+            LOG.error(message);
+            throw new IllegalStateException(message);
+        }
+        try {
+            SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        LOG.debugf("Retrying connection to %s...", queueName);
+        run();
+    }
 }
